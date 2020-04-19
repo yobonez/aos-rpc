fd4c1d1dc3d6b492fa16a65feea9344897f5946e
yobonez <59935203+yobonez@users.noreply.github.com>
2020-04-19 16:55:05 +0200

    Some fixes
    - Added and fixed some comments
    - Fixed server status updating. Server info values were always assigned
    outside of the updating loop, so that caused server info not updating
    for presence at all
    - Added localhost to json serverlist, but only for
    informing others that you are playing on localhost as server name,
    because i dont know how to gather server info that you are on from the
    client itself, i dont see any way for that for now

--- presence.py
+++ presence.py
@@ -9,49 +9,58 @@ AOS_RPC = Presence(CLIENT_ID)
 AOS_RPC.connect()
 try:
     def fetch_server(cmdline_again, key):
-        # Request json serverlist.
+        # Request json serverlist that buildandshoot hosts.
         serverlist_url = "https://services.buildandshoot.com/serverlist.json"
         req = urllib.request.urlopen(serverlist_url)
         data = req.read()
         enc = req.info().get_content_charset('utf-8')
         json_obj = json.loads(data.decode(enc))
+
+        # Add localhost identifier to serverlist:
+        with open("localhost.json") as f:
+            data = json.load(f)
+        json_obj.append(data)
+
         count = 0
 
-        # Removing "/" at the end of the server identifier to avoid an index out of range error 
-        # that throws for some reason idk
-        server_name = cmdline_again.strip("/")
+        # Removing "/" at the end of the server identifier to avoid an index out of range error
+        # that is being thrown for some reason idk
+        server_identifier = cmdline_again.strip("/")
 
         while True:
-            # If the identifier from the serverlist is valid with the current server i play,
-            # get the requested in function key from serverlist. For example 'name' or 'game_mode'and return it
-            if json_obj[count]['identifier'] == server_name:
-                key = json_obj[count][key]
-                return key
+            # If the identifier from the serverlist is valid with the current server player plays,
+            # get the requested key in function "fetch_server", compare it with key
+            # from serverlist and return it.
+            # For example key is the 'name' or 'game_mode'.
+            if json_obj[count]['identifier'] == server_identifier:
+                variable = json_obj[count][key]
+                return variable
             else:
                 count += 1
 
     def keep_alive(pid, cmdline):
         print('Process found. Starting presence.')
         start_time = time.time()
-        server_name = fetch_server(cmdline, 'name') # request server name from serverlist
-        server_map = fetch_server(cmdline, 'map') # same for here but for map etc.
-        server_game_mode = fetch_server(cmdline, 'game_mode')
-        server_players_current = fetch_server(cmdline, 'players_current')
-        server_players_max = fetch_server(cmdline, 'players_max')
         while pid is not None:
-            print('Checking if the process is still alive: ', pid)
             # Check if the process still exists. If not, then go back to process searching.
             if psutil.pid_exists(pid) is False:
                 print('Process was closed. Clearing presence.')
                 AOS_RPC.clear(pid=pid)
                 scan_for_process()
             print('Updating presence.')
+
+            server_name = fetch_server(cmdline, 'name') # request server name from serverlist
+            server_map = fetch_server(cmdline, 'map') # same for here but for map etc.
+            server_game_mode = fetch_server(cmdline, 'game_mode')
+            server_players_current = fetch_server(cmdline, 'players_current')
+            server_players_max = fetch_server(cmdline, 'players_max')
+
             print(AOS_RPC.update(pid=pid,
                                  details=server_name,
-                                 state='Map: {} ({})\nPlayers:{}/{}'.format(server_map,
-                                                                            server_game_mode,
-                                                                            server_players_current,
-                                                                            server_players_max),
+                                 state='Map: {} ({})\nPlayers: {}/{}'.format(server_map,
+                                                                             server_game_mode,
+                                                                             server_players_current,
+                                                                             server_players_max),
                                  start=start_time,
                                  large_image='ace_of_spades1',
                                  large_text='Ace of Spades v0.75'))
@@ -59,23 +68,21 @@ try:
 
     print('Waiting for a process...')
     def scan_for_process():
-        ps_name = None
         ps_pid = None
-        while ps_name is None:
+        while True:
             # Iterate thru processes to find one that matches our wanted one
-            # and assign important variables for further functions
+            # and assign important variables for further functions that will be executed
             for p in psutil.process_iter(['name', 'pid', 'cmdline']):
                 if p.info['name'] == 'client.exe':
-                    ps_name = p.info['name']
                     ps_pid = p.info['pid']
-                    ps_cmdline = p.info['cmdline'][1] # This is the server identifier "aos://XXXXXXXXX:XXXXX"
+                    ps_cmdline = p.info['cmdline'][1] # This is the server identifier "aos://XXXXXXXXXX:XXXXX"
                     keep_alive(ps_pid, ps_cmdline)
                 else:
-                    #print('Searching..')
                     time.sleep(0.05)
 
     if __name__ == "__main__":
+        # Start script
         scan_for_process()
 except KeyboardInterrupt:
-    print('Recived KeyboardInterrupt. Closing connection.')
+    print('Interrupt caught. Closing connection.')
     AOS_RPC.close()
